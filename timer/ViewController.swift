5a4151c80317c292cd49463e7da21e1a86d8d834
Baigalibbb <baigali.bakytzhanov26@fizmat.kz>
2023-10-30 21:45:05 +0600

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by Tenizbayev Bolat on 30.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
